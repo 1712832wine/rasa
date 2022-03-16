f0d772b30d4b5446e925e2ffe6dc7ccc053eb53f
1712832wine <nguyenhuutri31081999nht@gmail.com>
2022-03-16 12:26:06 +0700

    ok

--- actions/actions.py
+++ actions/actions.py
@@ -25,118 +25,16 @@ from nltk.corpus import stopwords
 bert = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
 
 
-class ActionMedical(Action):
+class ActionGoodbye(Action):
 
     def name(self) -> Text:
-        return "action_medical"
-
-    def remove_numbers(self, text):
-        result = re.sub(r'\d+', '', text)
-        return result
-
-    def remove_whitespace(self, text):
-        return " ".join(text.split())
-
-    def remove_punctuation(self, text):
-        translator = str.maketrans('', '', string.punctuation)
-        return text.translate(translator)
-
-    def remove_stopwords(self, text):
-        stop_words = set(stopwords.words("vietnamese"))
-        word_tokens = word_tokenize(text, format='text').split(' ')
-        filtered_text = [word.replace('_', ' ')
-                         for word in word_tokens if word not in stop_words]
-        if filtered_text:
-            return ' '.join(filtered_text)
-        else:
-            return text
-
-    def preprocess(self, text):
-        text = text.strip().lower().rstrip('\n')
-        text = self.remove_numbers(text)
-        text = self.remove_whitespace(text)
-        text = self.remove_punctuation(text)
-        # text = self.remove_stopwords(text)
-        return text
-
-    def compare(self, input, data_src):
-        # encode  1
-        emb_1 = bert.encode(input)
-        # encode  2
-        emb_2 = bert.encode(data_src)
-        # get similarity score
-        scores = util.dot_score(emb_1, emb_2)
-        return scores
-
-    def get_questions(self, text):
-        questions = []
-        pattern = re.compile(r'- -.*\n')
-        matches = pattern.finditer(text)
-        for match in matches:
-            s = match.group(0)[4:]
-            s = self.preprocess(s)
-            questions.append(s)
-        return questions
-
-    def get_answers(self, text):
-        answers = []
-        pattern = re.compile(r'  - .*\n')
-        matches = pattern.finditer(text)
-        for match in matches:
-            s = match.group(0)[4:]
-            answers.append(s)
-        return answers
-
-    def get_file(self, x):
-        switcher = {
-            'béo phì': '__beo_phi.yml',
-            'covid': '__covid.yml',
-            'da liễu': '__da_lieu.yml',
-            'đau mắt đỏ': '__dau_mat_do.yml',
-            'đau thần kinh tọa': '__dau_than_kinh_toa.yml',
-            'hạ canxi máu': '__ha_canxi_mau.yml',
-            'lao phổi': '__lao_phoi.yml',
-            'mụn trứng cá': '__mun_trung_ca.yml',
-            'quai bị, sâu răng': '__quai_bi_sau_rang.yml',
-            'tai-mũi-họng': '__tai_mui_hong.yml',
-            'tay-chân-miệng': '__tay_chan_mieng.yml',
-            'thận': '__than.yml',
-            'thủy đậu': '__thuy_dau.yml',
-            'tim mạch': '__tim_mach.yml',
-            'viêm đường hô hấp': '__viem_duong_ho_hap.yml',
-            'viêm xoang': '__viem_xoang.yml',
-        }
-        return switcher.get(x, "nothing")
-
-    def get_answer(self, input, filename):
-        with open(filename, encoding='utf-8') as f:
-            text = f.read()
-        data_src = self.get_questions(text)
-        data_dest = self.get_answers(text)
-        scores = self.compare(self.preprocess(input), data_src)
-        max_value = max(scores[0])
-        max_index = scores[0].tolist().index(max_value)
-        return data_dest[max_index]
+        return "action_goodbye"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # get intent
-        intent = tracker.latest_message.get("intent").get("name")
-        # get message input
-        message = tracker.latest_message.get("text")
-        temp = self.get_file(intent)
-
-        if temp != 'nothing':
-            # get filename
-            filename = './data/medical/' + temp
-            # get answer
-            answer = self.get_answer(message, filename)
-            dispatcher.utter_message(text=answer)
-        else:
-            dispatcher.utter_message(
-                text='Xin lỗi mình không biết trả lời câu này')
-        return []
+        dispatcher.utter_message(text='Tạm biệt')
+        return [AllSlotsReset()]
 
 # -------------------------------------------------------------------------
 # -------------------------------------------------------------------------
